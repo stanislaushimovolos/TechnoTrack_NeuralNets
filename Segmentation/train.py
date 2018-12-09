25ecff779e869a7fcbb915bb683d9703d2ddde7b
stanislaushimovolos <super.stas.stanislaw@gmail.com>
2018-12-09 15:37:57 +0300

    replaced train.py

--- Segmentation/train.py
+++ Segmentation/train.py
@@ -5,35 +5,28 @@ import os
 from tqdm import *
 import numpy as np
 from torch import nn
+import torch
 
 import torch.utils.data as dt
 import carvana_dataset as cv
 
-log = './log/'
-train = 'train/'
-train_masks = 'train_masks/'
-test = 'test/'
-test_masks = 'test_masks/'
 
-if os.path.exists(log) == False:
-    os.mkdir(log)
-tb_writer = SummaryWriter(log_dir='log')
-
-def train_net(net, net_lr=0.01, useCuda=True, n_epoch=100):
+def train_net(net, ds, ds_test, net_lr=0.01, useCuda=True, n_epoch=100, batch_sz=32):
+    
+    if os.path.exists('./log/') == False:
+        os.mkdir('./log/')
+    tb_writer = SummaryWriter(log_dir='log')
     
     m = net
-    criterion =  nn.MSELoss()
+    criterion =  torch.nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(m.parameters(), net_lr)
 
     if useCuda == True:
         m = m.cuda()
         criterion= criterion.cuda()
 
-    ds = cv.CarvanaDataset(train, train_masks)
-    ds_test = cv.CarvanaDataset(test, test_masks)
-
-    dl      = dt.DataLoader(ds, shuffle=True, num_workers=4, batch_size=16)
-    dl_test = dt.DataLoader(ds_test, shuffle=False, num_workers=4, batch_size=16)
+    dl      = dt.DataLoader(ds, shuffle=True, num_workers=4, batch_size=batch_sz)
+    dl_test = dt.DataLoader(ds_test, shuffle=False, num_workers=4, batch_size=batch_sz)
 
     global_iter = 0
     for epoch in range(0, n_epoch):
@@ -64,8 +57,9 @@ def train_net(net, net_lr=0.01, useCuda=True, n_epoch=100):
 
         tb_out = np.random.choice(range(0, len(dl_test)), 3 )
         for iter, (i, t) in enumerate(tqdm(dl_test)):
-            i = Variable(i, volatile = True)
-            t = Variable(t, volatile = True)
+            with torch.no_grad():
+                i = Variable(i)
+                t = Variable(t)
             if useCuda :
                 i = i.cuda()
                 t = t.cuda()
